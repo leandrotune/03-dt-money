1c400ec843653cdecd2ce2b4a50d5439e7c8d5b9
Leandro Antunes <leandro.tune@hotmail.com>
2022-08-21 21:31:49 -0300

    Componente: Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components"
+import { Transactions } from "./pages/Transactions"
 import { GlobalStyles } from "./styles/global"
 import { defaultTheme } from "./styles/themes/default"
 
@@ -6,8 +7,8 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
+      <Transactions />
       
-      <h1>Hello world!</h1>
     </ThemeProvider>
   )
 }
