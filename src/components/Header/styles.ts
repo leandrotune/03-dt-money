ca242a18e1211101a57eedcd0b6b326655132551
Leandro Antunes <leandro.tune@hotmail.com>
2022-08-22 12:42:07 -0300

    Componente: Summary

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -5,7 +5,7 @@ export const HeaderContainer = styled.header`
   padding: 2.5rem 0 7.5rem;
 `
 
-export const HeaderContent = styled.div`
+export const HeaderContent = styled.section`
   width: 100%;
   max-width: 1120px;
   margin: 0 auto;
